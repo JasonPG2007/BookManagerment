d46a8076da6b9bfbe4423da8e1e7f7215bac8a12
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-02-08 09:04:30 +0700

    Update 8/2

--- WebMVC_API/Controllers/HomeController.cs
+++ WebMVC_API/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Common;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
 using System.Net;
@@ -17,48 +16,9 @@ namespace WebMVC_API.Controllers
         // GET: HomeController
         public ActionResult Index()
         {
-            //MailUtils.SendMailGoogleSmtp("ngocdng08@gmail.com", "baoandng07@gmail.com", "Hello", "Demo", "ngocdng08@gmail.com", "baoan123.cntt");
             var list = bookRepository.GetBooks();
             return View(list);
         }
-        //[HttpPost]
-        //public ActionResult Index(string receiver, string subject, string message)
-        //{
-        //    try
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            var senderEmail = new MailAddress("baoandng07@gmail.com", "Jamil");
-        //            var receiverEmail = new MailAddress(receiver, "Receiver");
-        //            var password = "BAOan@67899";
-        //            var sub = subject;
-        //            var body = message;
-        //            var smtp = new SmtpClient
-        //            {
-        //                Host = "smtp.gmail.com",
-        //                Port = 587,
-        //                EnableSsl = true,
-        //                DeliveryMethod = SmtpDeliveryMethod.Network,
-        //                UseDefaultCredentials = false,
-        //                Credentials = new NetworkCredential(senderEmail.Address, password)
-        //            };
-        //            using (var mess = new MailMessage(senderEmail, receiverEmail)
-        //            {
-        //                Subject = subject,
-        //                Body = body
-        //            })
-        //            {
-        //                smtp.Send(mess);
-        //            }
-        //            return View();
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        ViewBag.Error = "Some Error";
-        //    }
-        //    return View();
-        //}
         // GET: HomeController/Details/5
         public ActionResult Details(int id)
         {
